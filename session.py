e00cb58f0c062a2004cda7167b7b8a1fd297ff1b
Stackexpress-Shreyansh <shreyansh.dubey@stackexpress.com>
2023-04-28 02:27:13 +0530

    terminal chat

--- session.py
+++ session.py
@@ -2,6 +2,7 @@ import os
 import openai
 import signal
 import sys
+import time
 import pdb
 from dotenv import load_dotenv
 
@@ -26,27 +27,33 @@ def get_response(input_text):
     prompt = session_messages.copy()
     prompt.append({"role": "user", "content": input_text})
     
-    response = openai.ChatCompletion.create(
-        model=model_engine,
-        messages=prompt,
-        temperature=0.7,
-        max_tokens=1024,
-        stop=None,
-        timeout=60,
-    )
-    message = response.choices[0]['message']['content']
-    # pdb.set_trace()
-    session_messages.append({"role": "assistant", "content": message})
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_engine,
+            messages=prompt,
+            temperature=0.7,
+            max_tokens=1024,
+            stop=None,
+            timeout=60,
+        )
+
+        message = response.choices[0]['message']['content']
+        session_messages.append({"role": "assistant", "content": message})
+        
+    except openai.error.RateLimitError:
+        print('============= Rate limit reached. Waiting for 20 seconds... =================')
+        time.sleep(20)
+        message = "Please send your message again...."
+    
     return message
 
 while True:
     try:
+        print('-----------------------------------------------------')
         user_input = input("You: ")
         response = get_response(user_input)
-        # pdb.set_trace()
-        assistant_response = next((msg["content"] for msg in reversed(session_messages) if msg["role"] == "assistant" and msg["content"]), None)
-        if assistant_response:
-            print("Assistant:", assistant_response)
+        print("Assistant: {}".format(response))
+        print('-----------------------------------------------------')
     
     except KeyboardInterrupt:
         signal_handler(signal.SIGINT, None)
