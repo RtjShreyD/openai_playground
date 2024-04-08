329f4205dd58cdbdf7fee712c9a859c5a2323017
Shreyans Dube <shreyanshece1041@gmail.com>
2024-04-08 23:51:13 +0530

    Update talk_to_gpt.py

--- openai_examples/talk_to_gpt.py
+++ openai_examples/talk_to_gpt.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 import openai
 import playsound
@@ -15,6 +16,9 @@ load_dotenv()
 
 r = sr.Recognizer()
 openai.api_key = os.getenv("API_KEY")
+session_messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
 
 
 def text_to_voice_gtts(text: str):
@@ -33,13 +37,40 @@ def text_to_voice_native(text: str):
     engine.say(text)
     engine.runAndWait()
 
+def get_response(input_text):
+    chat_gpt3_model_engine = "gpt-3.5-turbo" # this is the replacemenet model of davinci
+    global session_messages
+    
+    prompt = session_messages.copy()
+    prompt.append({"role": "user", "content": input_text})
+    
+    try:
+        # import pdb
+        # pdb.set_trace()
+        response = openai.chat.completions.create(
+            model=chat_gpt3_model_engine,
+            messages=prompt
+        )
+        # print(response.choices[0].message.content)
+        message = response.choices[0].message.content
+        session_messages.append({"role": "assistant", "content": message})
+        
+    except Exception as e:
+        print(e)
+        print('============= Rate limit reached. Waiting for 20 seconds... =================')
+        time.sleep(20)
+        message = "Please send your message again...."
+    
+    return message
 
 def get_gpt_response(prompt: str) -> Text:
-    chat_gpt3_model_engine = "text-davinci-003"
+    chat_gpt3_model_engine = "gpt-3.5-turbo-instruct" # this is the replacemenet model of davinci
     results = []
-    
+    # import pdb
+    # pdb.set_trace()
+    print(prompt)
     # Stream response from ChatGpt
-    for resp in openai.Completion.create(engine=chat_gpt3_model_engine, prompt=prompt, max_tokens=512, n=1, stop=None, temperature=0.5, stream=True):
+    for resp in openai.chat.completions.create(model=chat_gpt3_model_engine, messages=prompt, stream=True):
         text = resp.choices[0].message
         results.append(text)
         sys.stdout.write(text)
@@ -63,19 +94,21 @@ def main():
                 input_prompt = input_prompt.lower()
 
                 print("User :", input_prompt)
-                prompt_resp_text = get_gpt_response(input_prompt)
+                prompt_resp_text = get_response(input_prompt)
+                print(prompt_resp_text)
 
                 # Uncomment below to speak via the gtts engine (latency high, quality high)
-                # text_to_voice_gtts(prompt_resp_text)
+                text_to_voice_gtts(prompt_resp_text)
 
                 # Using the native pyttsx engine (latency low, quality low)
-                text_to_voice_native(prompt_resp_text)
+                # text_to_voice_native(prompt_resp_text)
 
         except Exception as e:
             print("Unable to request results from ChatGPT - {0}".format(e))
 
 
 if __name__ == '__main__':
+    # response = get_response("Hello")
     device_microphones = sr.Microphone.list_microphone_names()
     if device_microphones:
         for index, name in enumerate(device_microphones):
@@ -83,3 +116,4 @@ if __name__ == '__main__':
         main()
     else:
         print("No audio input devices found")
+        
